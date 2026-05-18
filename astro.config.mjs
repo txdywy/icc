08acbbd60bae4b6ea17413854200146ac18d5279
ROn <geniusron@gmail.com>
2026-05-18 09:53:22 +0800

    Configure GitHub Pages automation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.github.io',
+  site: 'https://txdywy.github.io',
+  base: '/icc',
   integrations: [tailwind()],
   output: 'static',
 });
